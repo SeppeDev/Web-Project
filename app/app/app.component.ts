23772e2dd5dfebac6ecc7ef03b592f986c18a3b3
Benno Meysmans <benno_njord@hotmail.com>
2016-10-10 12:36:20 +0200

    Update to newest ng2

--- app/app/app.component.ts
+++ app/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
+import { Component }    from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-    selector: "my-app",
+    selector: "chorehub-app",
     template: "<h1>My First Angular App</h1>"
 })
 export class AppComponent { }
