5a8c3748b1510b96735b6ff1b01d67a6faf878d4
SeppeDev <goossens.seppe@hotmail.com>
2016-10-27 23:05:01 +0200

    Get more Auth0 user info

--- api/ChoreHub/Startup.cs
+++ api/ChoreHub/Startup.cs
@@ -8,6 +8,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+
 using Microsoft.EntityFrameworkCore;
 using ChoreHub.DAL;
 
@@ -35,14 +43,19 @@ namespace ChoreHub
             // Add framework services.
             services.AddMvc();
 
+            // Add the Auth0 Settings object so it can be injected
+            services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
+
             services.AddDbContext<ChoreHubContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddSingleton<IChoreRepository, ChoreRepository>();
             services.AddSingleton<IImageRepository, ImageRepository>();
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddSingleton<IReviewRepository, ReviewRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ChoreHubContext context)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IOptions<Auth0Settings> auth0Settings, ChoreHubContext context)
         {
             // Auth0
             var options = new JwtBearerOptions
@@ -55,6 +68,61 @@ namespace ChoreHub
 
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+
+            //Auth0 get user info
+            var OpenIdConnectionOptions = new OpenIdConnectOptions("Auth0")
+            {
+                // Set the authority to your Auth0 domain
+                Authority = $"https://{auth0Settings.Value.Domain}",
+
+                // Configure the Auth0 Client ID and Client Secret
+                ClientId = auth0Settings.Value.ClientId,
+                ClientSecret = auth0Settings.Value.ClientSecret,
+
+                // Do not automatically authenticate and challenge
+                AutomaticAuthenticate = false,
+                AutomaticChallenge = false,
+
+                // Set response type to code
+                ResponseType = "code",
+
+                // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
+                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
+                CallbackPath = new PathString("/signin-auth0"),
+
+                // Configure the Claims Issuer to be Auth0
+                ClaimsIssuer = "Auth0",
+
+                // Saves tokens to the AuthenticationProperties
+                SaveTokens = true,
+
+                Events = new OpenIdConnectEvents
+                {
+                    OnTicketReceived = aContext =>
+                    {
+                        // Get the ClaimsIdentity
+                        var identity = aContext.Principal.Identity as ClaimsIdentity;
+                        if (identity != null)
+                        {
+                            // Add the Name ClaimType. This is required if we want User.Identity.Name to actually return something!
+                            if (!aContext.Principal.HasClaim(c => c.Type == ClaimTypes.Name) &&
+                                identity.HasClaim(c => c.Type == "name"))
+                                identity.AddClaim(new Claim(ClaimTypes.Name, identity.FindFirst("name").Value));
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                }
+            };
+            OpenIdConnectionOptions.Scope.Clear();
+            OpenIdConnectionOptions.Scope.Add("openid");
+            OpenIdConnectionOptions.Scope.Add("name");
+            OpenIdConnectionOptions.Scope.Add("email");
+            OpenIdConnectionOptions.Scope.Add("picture");
+            app.UseOpenIdConnectAuthentication(OpenIdConnectionOptions);
+
+
 
             // Entity
             DbInitializer.Initialize(context);
