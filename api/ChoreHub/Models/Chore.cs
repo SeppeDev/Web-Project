b88ca867b7265afe81d40264556bfe252280d61e
SeppeDev <goossens.seppe@hotmail.com>
2016-11-04 16:47:48 +0100

    Fixed Description length nVarChar(MAX)

--- api/ChoreHub/Models/Chore.cs
+++ api/ChoreHub/Models/Chore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,7 +21,7 @@ namespace ChoreHub.Models
         public Category Category { get; set; }
 
         [Required]
-        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(MAX)")]
         public string Description { get; set; }
     }
 }
--- api/ChoreHub/Models/User.cs
+++ api/ChoreHub/Models/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,7 +29,7 @@ namespace ChoreHub.Models
         public string Email { get; set; }
 
         [Required]
-        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(MAX)")]
 	    public string Description { get; set; }
 
         [DefaultValue(false)]
