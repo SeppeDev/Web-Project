c0c790f91c8022ce142d5d1d62f57b3b3b4fbbdb
SeppeDev <goossens.seppe@hotmail.com>
2016-11-04 16:32:47 +0100

    Add model Validation

--- api/ChoreHub/Models/Chore.cs
+++ api/ChoreHub/Models/Chore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace ChoreHub.Models
     public class Chore
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public User User { get; set; }
+
+        [Required]
         public Category Category { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Description { get; set; }
     }
 }
--- api/ChoreHub/Models/User.cs
+++ api/ChoreHub/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +10,34 @@ namespace ChoreHub.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string UserId { get; set; }
+
+        [Required]
+        [MaxLength(46)]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(46)]
         public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(62)]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
 	    public string Description { get; set; }
+
+        [DefaultValue(false)]
         public bool IsAdmin { get; set; }
+
+        [Required]
         public bool IsPublic { get; set; }
+
+        [Required]
         public Image Image { get; set; }
     }
 }
